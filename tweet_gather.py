e5af27daea2d5b13bd2640f56253c243b5a6e280
Lijia Wang <lwa19@users.noreply.github.com>
2020-02-10 20:09:19 -0600

    end live share session push

--- tweet_gather.py
+++ tweet_gather.py
@@ -13,15 +13,19 @@
 # http://docs.tweepy.org/en/latest/api.html
 
 # import necessary libraries
+from datetime import datetime
 import tweepy as tw
+import pandas as pd
 import json
 
 # obtain consumer and access keys from json
 with open('twitter_credentials.json', 'r') as f:
     keys = json.load(f)
 
-auth = tw.OAuthHandler(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'])
-auth.set_access_token(keys['ACCESS_TOKEN'], keys['ACCESS_SECRET'])
+auth = tw.OAuthHandler(keys['CONSUMER_KEY'], keys['CONSUMER_SECRET'], \
+    secure=True)
+auth.set_access_token(keys['ACCESS_TOKEN'], keys['ACCESS_SECRET'], \
+    secure=True)
 
 # authenticate API
 api = tw.API(auth)
@@ -51,12 +55,50 @@ for tweet in public_tweets:
 # After this we can search tweets by hashtag and save the results
 # https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets
 
-def search_words(ui_input, day_since=None, limit=100):
+def get_data(promoted=False):
+    '''
+    Function that 
+    '''
+    # Get trends for a location (loop over all locations)
+    # Note: Need to have a list of woeids as our default batch collect for the whole US
+    # Get tweets for each trend (loop over all trends)
+
+def get_trends(woeid):
+    '''
+    Get the current trends for a particular location. Saves json file with current date and time
+    and returns trend data as a pandas dataframe.
+
+    This function can only get current trends. There is no way to get past trends without using
+    third party archive websites, which only save trend names.
+
+    Inputs:
+        woeid (int): "where on earth id" a 32-bit integer identifier for any location on earth
+        incl_promoted: boolean
+
+    Outputs: 
+        json file saved with current date and time
+        trend_info: pandas dataframe with 
+    '''
+    trends = tw.Cursor(api.trends_place, woeid)
+
+    dt = datetime.now()
+    formatted_dt = dt.strftime("%Y/%m/%d_%H:%M")
+    filename = "trends_{}.json".format(formatted_dt)
+
+    with open(filename, "w") as output_file:
+        json.dump(trends, output_file)
+
+    trend_info = pandas.read_json(trends)
+    trend_info = trend_info.drop(columns=["url", "query"])
+
+    return None
+
+def search_words(input_hashtag, day_since=None, limit=100):
     '''
     Use the cursor function to gather a collection of tweets associated with a hashtag
 
     Inputs:
-        ui_input (string): hashtag that the user entered (*potential: we could check to 
+        input_hashtag (string): hashtag that the user entered (*potential: we could check to 
             see if they included '#')
         date_since (???) : optional parameter; specifies how long back in the tweet 
             history we're restricting the search to (*replace None with appropriate default value)
@@ -67,9 +109,18 @@ def search_words(ui_input, day_since=None, limit=100):
     tweets = tw.Cursor(api.search,
               q=search_words,
               lang="en",
-              since=day_since).items(limit)
+              #since=day_since
+              ).items(limit)
+    print(keys)
+              
+    return tweets
 
 # The results will be in a JSON file
 # https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/intro-to-tweet-json
 
+
+    
+  
+# Functions we need:
+# 
 
