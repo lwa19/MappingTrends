44a11ac680c0a36ccf6842978158b295df6970fb
Kunal <kunalsmahajan@gmail.com>
2020-03-12 11:50:33 -0500

    More options for filename

--- mapper.py
+++ mapper.py
@@ -10,7 +10,7 @@ import seaborn as sns
 from descartes import PolygonPatch
 import random
 
-print("yeet")
+#print("yeet")
 #plot1()
 state_code = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
@@ -34,9 +34,9 @@ class Shapefile:
         Read in a shapefile using its path
         By default uses a shapefile of the 50 US States + DC
         '''
-        print("a")
+        #print("a")
         self.gdf = geopandas.GeoDataFrame.from_file(shp_path)
-        print(self.gdf)
+        #print(self.gdf)
 
     
         self.sf = shp.Reader(shp_path)
@@ -66,21 +66,56 @@ class Shapefile:
 
     def plot_data(self, file_name = "test_file", show_plot = True):
         plot_list = []
+        output_path =[]
         for col in self.cols:
             plot_list.append(build_plot(self, file_name, col, show_plot))
 
         if len(plot_list) == 1:
+            assert type(file_name) == str
             plot_list[0].savefig('./Plot_pngs/' + file_name + '.png')
+            output_path.append('./Plot_pngs/' + file_name + '.png')
         else:
-            print(plot_list)
+            assert (type(file_name) == list and type(file_name[0]) == str) or type(file_name) == str
+            names_list = []
+            if type(file_name) == str:
+                for i in range(len(plot_list)):
+                    names_list.append(file_name + "("+str(i) + ")")
+            elif type(file_name) == list:
+                names_list = file_name[:]
+                #print(names_list)
+
+            #print(names_list)
             for i, plot in enumerate(plot_list):
-                plot.savefig('./Plot_pngs/' + file_name + "("+str(i)+").png")
+                # plot.savefig('./Plot_pngs/' + file_name + "("+str(i)+").png")
+                # output_path.append('./Plot_pngs/' + file_name + "("+str(i)+").png")
+                plot.savefig('./Plot_pngs/' + names_list[i]+".png")
+                output_path.append('./Plot_pngs/' + names_list[i]+".png")
+        return output_path
 
-        return plot_list
 
 
 
 
+def map_data(dict_data, col_names, file_name, show_plot = True):
+    '''
+    All-in-one function that creates new Shapefile object, adds data to
+        geodataframe and plots data.
+    Inputs: 
+            dict_data (list of dicts)
+            col_names (list of strings)
+            file_name: Determines names of written .png files, some flexibility on datatype
+                (string): basefilename, if multiple images are to be made it appends an integer
+                (list): custom filenames
+            show_plot (bool): Choice on whether or not to display plot
+                              (the file(s) are still saved regardless of show_plot)
+    Output:
+        paths (list of strings): representing filepaths that images are written to
+
+    '''
+    map1 = Shapefile()
+    map1.add_data(dict_data,col_names)
+    paths =  map1.plot_data(file_name, show_plot)
+    return paths
 
 
 def build_plot(shapefile_object, file_name, col = 'count', show_plot = True):
@@ -88,7 +123,9 @@ def build_plot(shapefile_object, file_name, col = 'count', show_plot = True):
     Main function to build plot in matplotlib
     Inputs:
         Shapefile_object (object of Shapefile class)
-        file_name (string): What you want to name the file
+        file_name: Determines names of written .png files, some flexibility
+            (string): basefilename, if multiple images are to be made it appends an integer
+            (list): custom filenames
         col (string): Name of the column data to plot
         show_plot (bool): Choice on whether or not to display plot
                           (the file(s) are still saved regardless of show_plot)
@@ -115,8 +152,8 @@ def build_plot(shapefile_object, file_name, col = 'count', show_plot = True):
 
         #ax.add_patch(PolygonPatch(poly, fc=BLUE, ec=BLUE, alpha=0.5, zorder=2 ))
         #color_sq =['#b3cde0','#6497b1','#005b96','#03396c','#011f4b']
-        print(test['STATE_ABBR'][i])
-        print("color_map: " + str(color_map[test['STATE_ABBR'][i]]))
+        ##print(test['STATE_ABBR'][i])
+        ##print("color_map: " + str(color_map[test['STATE_ABBR'][i]]))
         ax.add_patch(PolygonPatch(poly, fc = color_sq[color_map[test['STATE_ABBR'][i]]], zorder=2 ))
         ax.axis('scaled')
     if show_plot:
@@ -133,20 +170,20 @@ def get_color_bins(geoframe, col = 'count'):
     list_val = []
     list_none = []
     for row in geoframe.iterrows():
-        print(row[1][col], type(row[1][col]))
+        ##print(row[1][col], type(row[1][col]))
         if pd.notna(row[1][col]): ##??check for null
             #temp_list.append((row[1]['STATE_ABBR'],row[1]['count']))
             list_key.append(row[1]['STATE_ABBR'])
             list_val.append(row[1][col])
         else:
-            print("appended none")
+            ##print("appended none")
             list_none.append(row[1]['STATE_ABBR'])
 
     assert len(list_key) == len(list_val)
-    print(list_val)
+    ##print(list_val)
     list_val = pd.qcut(list_val, q = 4, labels = [1,2,3,4])
 
-    print(list_key)
+    ##print(list_key)
 
     for i, state in enumerate(list_key):
         temp_map[state] = list_val[i]
@@ -187,10 +224,10 @@ def join_dict_data(state_dicts, geoframe, cols = ['count']):
     # print(list_of_lists)
     # return list_of_lists
     for i, col in enumerate(cols):
-        print(i, col)
+        #print(i, col)
         #print(len(geoframe[col]))
-        print(len(list_of_lists[i]))
-        print(list_of_lists)
+        #print(len(list_of_lists[i]))
+        #print(list_of_lists)
         geoframe[col] = list_of_lists[i]
 
     return geoframe
